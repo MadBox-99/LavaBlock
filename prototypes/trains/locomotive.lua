890ffe57db95e459bd880fe3545f519b9c5241d3
Zoltán Tamás Szabó <72586295+MadBox-99@users.noreply.github.com>
2025-08-24 22:56:58 +0200

    Add locomotive recipe definition with ingredients and crafting details

--- prototypes/trains/locomotive.lua
+++ prototypes/trains/locomotive.lua
@@ -1,3 +1,19 @@
+local locomotive = table.deepcopy(data.raw["recipe"]["locomotive"])
+
+locomotive.ingredients = {
+    { type = "item", name = "electronic-circuit", amount = 10 },
+    { type = "item", name = "engine-unit",        amount = 20 },
+    { type = "item", name = "iron-gear-wheel",    amount = 1000 }, -- piece(s) / real-wagon: 10000
+    { type = "fluid", name = "lava",               amount = 100000 }
+}
+locomotive.category = "crafting-with-fluid"
+locomotive.energy_required = 60
+locomotive.results = {
+    { type = "item", name = "locomotive", amount = 1 },
+    { type = "item", name = "stone", amount = 50 }
+}
+locomotive.main_product = "locomotive"
+--[[ 
 local recipe = {
     type = "recipe",
     name = "locomotive",
@@ -13,6 +29,7 @@ local recipe = {
     },
     results = {
         { type = "item", name = "locomotive", amount = 1 },
-        { type = "item", name = "stone",      amount = 50 } } --slug --test-feature
-}
-data.raw["recipe"]["locomotive"] = recipe
+        { type = "item", name = "stone", amount = 50 }
+    }
+}  ]]
+data.raw["recipe"]["locomotive"] = locomotive
